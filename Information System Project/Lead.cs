1b8044fdca73605e690dda928dbdb705ae400668
Maya Adar <mayabee2@gmail.com>
2023-06-03 12:02:29 +0300

    added lead properties

---
+++ Information System Project/Lead.cs
@@ -0,0 +1,13 @@
+namespace WOPA;
+
+public class Lead
+{
+    private string companyName;
+    private string contact;
+    private string number;
+    private string email;
+    private DateTime dateOpened;
+    private LeadStatus status;
+    private int workStation;
+    private LeadSource source;
+}
