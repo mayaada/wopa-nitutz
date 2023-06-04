4ad431bf3efa9d596ef6f5695d1e607b0e6862cd
Maya Adar <mayabee2@gmail.com>
2023-06-04 18:04:10 +0300

    Init Leased item type

---
+++ WOPA/LeasedItemType.cs
@@ -0,0 +1,89 @@
+namespace WOPA;
+
+public class LeasedItemType
+{
+    
+    private string type;
+    private double price;
+    private int creditAmount;
+    private double deposit;
+
+    public LeasedItemType(string type, double price, int creditAmount, double deposit, bool isNew)
+    {
+        this.type = type;
+        this.price = price;
+        this.creditAmount = creditAmount;
+        this.deposit = deposit;
+        if (isNew)
+        {
+            createLeasedIteType();
+            Program.LeasedItemTypes.Add(this);
+        }
+    }
+
+    public string getType()
+    {
+        return this.type;
+    }
+
+    public double getPrice()
+    {
+        return this.price;
+    }
+
+    public int getCreditAmount()
+    {
+        return this.creditAmount;
+    }
+
+    public double getDeposit()
+    {
+        return this.deposit;
+    }
+
+
+    public void updateType(string type)
+    {
+        this.type = type;
+    }
+
+    public void updatePrice(double price)
+    {
+        this.price = price;
+    }
+
+    public void updateCreditAmount(int creditAmount)
+    {
+        this.creditAmount = creditAmount;
+    }
+
+    public void updateDeposit(double deposit)
+    {
+        this.deposit = deposit;
+    }
+
+
+    public void createLeasedItemType()
+    {
+        SqlCommand c = new SqlCommand();
+        c.CommandText = "EXECUTE dbo.Create_Leased_Item @type, @price, @creditAmount, @deposit";
+        c.Parameters.AddWithValue("@type", type);
+        c.Parameters.AddWithValue("@price", price);
+        c.Parameters.AddWithValue("@creditAmount", creditAmount);
+        c.Parameters.AddWithValue("@deposit", deposit);
+        SQL_CON SC = new SQL_CON();
+        SC.execute_non_query(c);
+    }
+
+    public void updateLeasedItemType()
+    {
+        SqlCommand c = new SqlCommand();
+        c.CommandText = "EXECUTE dbo.Update_Leased_Item @type, @price, @creditAmount, @deposit";
+        c.Parameters.AddWithValue("@type", type);
+        c.Parameters.AddWithValue("@price", price);
+        c.Parameters.AddWithValue("@creditAmount", creditAmount);
+        c.Parameters.AddWithValue("@deposit", deposit);
+        SQL_CON SC = new SQL_CON();
+        SC.execute_non_query(c);
+    }
+}
