1b31cc742edc05917f916c3a5285bbd54dd31d17
Maya Adar <mayabee2@gmail.com>
2023-06-04 18:14:21 +0300

    Update Lease Methods

--- WOPA/Lease.cs
+++ WOPA/Lease.cs
@@ -43,10 +43,87 @@ namespace WOPA
                 Program.Leases.Add(this);
             }
         }
-
-        private int addWorkStation(int numberOfWorkStations)
+        
+        //create getters for all attributes
+        public int getLeaseID()
         {
-            this.w
+            return this.leaseID;
+        }
+        
+        public string getSignerName()
+        {
+            return this.signerName;
+        }
+        
+        public DateTime getStartDate()
+        {
+            return this.startDate;
+        }
+        
+        public DateTime getEndDate()
+        {
+            return this.endDate;
+        }
+        
+        public int getTerminationNotice()
+        {
+            return this.terminationNotice;
+        }
+        
+        public string getTermsAndConditions()
+        {
+            return this.termsAndConditions;
+        }
+        
+        public Employee getSignedByEmployee()
+        {
+            return this.signedByEmployee;
+        }
+        
+        
+        public Tenant getSignedByTenant()
+        {
+            return this.signedByTenant;
+        }
+        
+        // create update for all attributes
+        
+        public void updateStartDate(DateTime startDate)
+        {
+            this.startDate = startDate;
+        }
+        
+        public void updateEndDate(DateTime endDate)
+        {
+            this.endDate = endDate;
+        }
+        
+        public void updateTerminationNotice(int terminationNotice)
+        {
+            this.terminationNotice = terminationNotice;
+        }
+        
+        public void updateTermsAndConditions(string termsAndConditions)
+        {
+            this.termsAndConditions = termsAndConditions;
+        }
+        
+        
+        
+                         public void createLease()
+        {
+            SqlCommand c = new SqlCommand();
+            c.CommandText = "EXECUTE dbo.Create_Lease @leaseID, @signerName, @startDate, @endDate, @terminationNotice, @termsAndConditions, @signedByEmployee, @signedByTenant";
+            c.Parameters.AddWithValue("@leaseID", leaseID);
+            c.Parameters.AddWithValue("@signerName", signerName);
+            c.Parameters.AddWithValue("@startDate", startDate);
+            c.Parameters.AddWithValue("@endDate", endDate);
+            c.Parameters.AddWithValue("@terminationNotice", terminationNotice);
+            c.Parameters.AddWithValue("@termsAndConditions", termsAndConditions);
+            c.Parameters.AddWithValue("@signedByEmployee", signedByEmployee);
+            c.Parameters.AddWithValue("@signedByTenant", signedByTenant);
+            SQL_CON SC = new SQL_CON();
+            SC.execute_non_query(c);
         }
     }
 }
