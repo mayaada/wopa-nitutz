3e62f79dfdf9881c27dc884871d4c92136ef0050
adiakriv <adiakri@post.bgu.ac.il>
2023-06-06 12:03:20 +0300

    new forms init

---
+++ EmployeeSignIn.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace nitutz
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Bahnschrift Condensed", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(295, 78);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(238, 56);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "התחבר למערכת";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(318, 274);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(215, 31);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(318, 217);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(215, 31);
+            textBox2.TabIndex = 3;
+            textBox2.TextChanged += textBox2_TextChanged_1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(576, 223);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 25);
+            label1.TabIndex = 4;
+            label1.Text = "שם משתמש";
+            label1.Click += label1_Click_1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(576, 280);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 25);
+            label2.TabIndex = 5;
+            label2.Text = "סיסמא";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(353, 362);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 6;
+            button1.Text = "המשך";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox1);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+    }
+}
--- mainForm.cs
+++ mainForm.cs
@@ -21,5 +21,22 @@ namespace nitutz
         {
 
         }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+            this.Hide();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
