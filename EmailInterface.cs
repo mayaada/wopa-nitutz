2e4f0335d388adedf44ab00d981a1c978dc2988a
Tamar Yosef Hay <tamaryos@post.bgu.ac.il>
2023-06-14 00:03:58 +0300

    email interface

--- EmailInterface.cs
+++ EmailInterface.cs
@@ -1,37 +1,41 @@
-using System;
-using System.Net;
-using System.Net.Mail;
+// using System;
+// using System.Net;
+// using System.Net.Mail;
 
-public class EmailSender
-{
-    public void SendEmail(string senderEmail, string recipientEmail, string subject, string body)
-    {
-        // Create a new MailMessage object
-        MailMessage mail = new MailMessage(senderEmail, recipientEmail);
+// namespace nitutz
+// {
 
-        // Set the subject and body of the email
-        mail.Subject = subject;
-        mail.Body = body;
+//     public class EmailSender
+//     {
+//         public void SendEmail(string senderEmail, string recipientEmail, string subject, string body)
+//         {
+//             // Create a new MailMessage object
+//             MailMessage mail = new MailMessage(senderEmail, recipientEmail);
 
-        // Create a new SmtpClient object
-        SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+//             // Set the subject and body of the email
+//             mail.Subject = subject;
+//             mail.Body = body;
 
-    
-        // Enable SSL/TLS encryption
-        smtpClient.EnableSsl = true;
+//             // Create a new SmtpClient object
+//             SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
 
-        try
-        {
-            // Send the email
-            smtpClient.Send(mail);
-            Console.WriteLine("Email sent successfully.");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Failed to send email. Error message: " + ex.Message);
-        }
-    }
-}
+
+//             // Enable SSL/TLS encryption
+//             smtpClient.EnableSsl = true;
+
+//             try
+//             {
+//                 // Send the email
+//                 smtpClient.Send(mail);
+//                 Console.WriteLine("Email sent successfully.");
+//             }
+//             catch (Exception ex)
+//             {
+//                 Console.WriteLine("Failed to send email. Error message: " + ex.Message);
+//             }
+//         }
+//     }
+// }
 
 
 
